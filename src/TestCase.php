5d0340c4f372105d79cb0d3e21132bc8f88b1500
crynobone <crynobone@gmail.com>
2017-02-12 23:04:09 +0800

    Bump orchestra/testbench and update PHPUnit namespace.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/TestCase.php
+++ src/TestCase.php
@@ -6,6 +6,7 @@ use Mockery;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Database\Eloquent\Model;
 use Orchestra\Testbench\Traits\WithFactories;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\WithoutEvents;
 use Orchestra\Testbench\Traits\ApplicationTrait;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -22,7 +23,7 @@ use Laravel\BrowserKitTesting\Concerns\MocksApplicationServices;
 use Laravel\BrowserKitTesting\Concerns\InteractsWithAuthentication;
 use Orchestra\Testbench\BrowserKit\Contracts\TestCase as TestCaseContract;
 
-abstract class TestCase extends \PHPUnit_Framework_TestCase implements TestCaseContract
+abstract class TestCase extends BaseTestCase implements TestCaseContract
 {
     use ApplicationTrait,
         InteractsWithContainer,
