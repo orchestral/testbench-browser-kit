672ad4e326aedd82bc3e1599cbc93f4685e56896
Mior Muhammad Zaki <crynobone@gmail.com>
2018-01-19 10:59:23 +0800

    Update to latest Testbench support.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/TestCase.php
+++ src/TestCase.php
@@ -6,6 +6,7 @@ use Mockery;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Database\Eloquent\Model;
+use Orchestra\Testbench\Concerns\Testing;
 use Illuminate\Foundation\Testing\WithFaker;
 use Orchestra\Testbench\Traits\WithFactories;
 use Illuminate\Console\Application as Artisan;
@@ -30,25 +31,15 @@ use Orchestra\Testbench\BrowserKit\Contracts\TestCase as TestCaseContract;
 
 abstract class TestCase extends BaseTestCase implements TestCaseContract
 {
-    use CreatesApplication,
-        InteractsWithContainer,
-        MakesHttpRequests,
+    use Testing,
         ImpersonatesUsers,
         InteractsWithAuthentication,
+        InteractsWithContainer,
         InteractsWithConsole,
         InteractsWithDatabase,
         InteractsWithSession,
-        MocksApplicationServices,
-        WithFactories,
-        WithLaravelMigrations,
-        WithLoadMigrationsFrom;
-
-    /**
-     * The Illuminate application instance.
-     *
-     * @var \Illuminate\Foundation\Application
-     */
-    protected $app;
+        MakesHttpRequests,
+        MocksApplicationServices;
 
     /**
      * The base URL to use while testing the application.
@@ -58,48 +49,33 @@ abstract class TestCase extends BaseTestCase implements TestCaseContract
     protected $baseUrl = 'http://localhost';
 
     /**
-     * The callbacks that should be run after the application is created.
-     *
-     * @var array
-     */
-    protected $afterApplicationCreatedCallbacks = [];
-
-    /**
-     * The callbacks that should be run before the application is destroyed.
-     *
-     * @var array
-     */
-    protected $beforeApplicationDestroyedCallbacks = [];
-
-    /**
-     * Indicates if we have made it through the base setUp function.
-     *
-     * @var bool
-     */
-    protected $setUpHasRun = false;
-
-    /**
      * Setup the test environment.
      *
      * @return void
      */
     protected function setUp()
     {
-        if (! $this->app) {
-            $this->refreshApplication();
-        }
+        $this->setUpTheTestEnvironment();
+    }
 
-        $this->setUpTraits();
+    /**
+     * Clean up the testing environment before the next test.
+     *
+     * @return void
+     */
+    protected function tearDown()
+    {
+        $this->tearDownTheTestEnvironment();
+    }
 
-        foreach ($this->afterApplicationCreatedCallbacks as $callback) {
-            call_user_func($callback);
-        }
-
-        Facade::clearResolvedInstances();
-
-        Model::setEventDispatcher($this->app['events']);
-
-        $this->setUpHasRun = true;
+    /**
+     * Boot the testing helper traits.
+     *
+     * @return array
+     */
+    protected function setUpTraits()
+    {
+        return $this->setUpTheTestEnvironmentTraits();
     }
 
     /**
@@ -112,113 +88,6 @@ abstract class TestCase extends BaseTestCase implements TestCaseContract
         putenv('APP_ENV=testing');
 
         $this->app = $this->createApplication();
-    }
-
-    /**
-     * Boot the testing helper traits.
-     *
-     * @return array
-     */
-    protected function setUpTraits()
-    {
-        $uses = array_flip(class_uses_recursive(static::class));
-
-        if (isset($uses[RefreshDatabase::class])) {
-            $this->refreshDatabase();
-        }
-
-        if (isset($uses[DatabaseMigrations::class])) {
-            $this->runDatabaseMigrations();
-        }
-
-        if (isset($uses[DatabaseTransactions::class])) {
-            $this->beginDatabaseTransaction();
-        }
-
-        if (isset($uses[WithoutMiddleware::class])) {
-            $this->disableMiddlewareForAllTests();
-        }
-
-        if (isset($uses[WithoutEvents::class])) {
-            $this->disableEventsForAllTests();
-        }
-
-        if (isset($uses[WithFaker::class])) {
-            $this->setUpFaker();
-        }
-
-        return $uses;
-    }
-
-    /**
-     * Clean up the testing environment before the next test.
-     *
-     * @return void
-     */
-    protected function tearDown()
-    {
-        if ($this->app) {
-            foreach ($this->beforeApplicationDestroyedCallbacks as $callback) {
-                call_user_func($callback);
-            }
-
-            $this->app->flush();
-
-            $this->app = null;
-        }
-
-        $this->setUpHasRun = false;
-
-        if (property_exists($this, 'serverVariables')) {
-            $this->serverVariables = [];
-        }
-
-        if (property_exists($this, 'defaultHeaders')) {
-            $this->defaultHeaders = [];
-        }
-
-        if (class_exists('Mockery')) {
-            if ($container = Mockery::getContainer()) {
-                $this->addToAssertionCount($container->mockery_getExpectationCount());
-            }
-
-            Mockery::close();
-        }
-
-        Carbon::setTestNow();
-
-        $this->afterApplicationCreatedCallbacks = [];
-        $this->beforeApplicationDestroyedCallbacks = [];
-
-        Artisan::forgetBootstrappers();
-    }
-
-    /**
-     * Register a callback to be run after the application is created.
-     *
-     * @param  callable  $callback
-     *
-     * @return void
-     */
-    protected function afterApplicationCreated(callable $callback)
-    {
-        $this->afterApplicationCreatedCallbacks[] = $callback;
-
-        if ($this->setUpHasRun) {
-            call_user_func($callback);
-        }
-    }
-
-    /**
-     * Register a callback to be run before the application is destroyed.
-     *
-     * @param  callable  $callback
-     *
-     * @return void
-     */
-    protected function beforeApplicationDestroyed(callable $callback)
-    {
-        array_unshift($this->beforeApplicationDestroyedCallbacks, $callback);
     }
 
     /**
