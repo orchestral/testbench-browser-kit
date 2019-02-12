a77b53e2452cd7ec030cb90d027ec80b47abbd3e
Mior Muhammad Zaki <crynobone@gmail.com>
2019-02-12 22:30:10 +0800

    Add tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/ApplicationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Orchestra\Testbench\BrowserKit\Tests;
+
+use Illuminate\Routing\Router;
+use Orchestra\Testbench\BrowserKit\TestCase;
+
+class ApplicationTest extends TestCase
+{
+    /** @test */
+    public function it_uses_testing_as_environment()
+    {
+        $this->assertEquals('testing', $this->app->environment());
+    }
+}
