cac1324960d575f8d03c2fffd8a2e64f186082d1
Mior Muhammad Zaki <crynobone@gmail.com>
2023-10-25 09:50:17 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -8,22 +8,22 @@ use Orchestra\Testbench\BrowserKit\TestCase;
 class RouteTest extends TestCase
 {
     /**
-     * Define environment setup.
+     * Define routes setup.
      *
-     * @param  Illuminate\Foundation\Application  $app
+     * @param  \Illuminate\Routing\Router  $router
      * @return void
      */
-    protected function getEnvironmentSetUp($app)
+    protected function defineRoutes($router)
     {
-        $app['router']->get('hello', ['as' => 'hi', 'uses' => function () {
+        $router->get('hello', ['as' => 'hi', 'uses' => function () {
             return 'hello world';
         }]);
 
-        $app['router']->get('goodbye', function () {
+        $router->get('goodbye', function () {
             return 'goodbye world';
         })->name('bye');
 
-        $app['router']->group(['prefix' => 'boss'], function (Router $router) {
+        $router->group(['prefix' => 'boss'], function (Router $router) {
             $router->get('hello', ['as' => 'boss.hi', 'uses' => function () {
                 return 'hello boss';
             }]);
@@ -33,7 +33,7 @@ class RouteTest extends TestCase
             })->name('boss.bye');
         });
 
-        $app['router']->resource('foo', 'Orchestra\Testbench\BrowserKit\Tests\Stubs\Controller');
+        $router->resource('foo', 'Orchestra\Testbench\BrowserKit\Tests\Stubs\Controller');
     }
 
     /** @test */
